7f982f74067d4b4b0b59f1a32f3ff304833abc4a
KDev <sk555av@gmail.com>
2019-04-17 11:01:19 +0700

    update font

--- src/contrainers/About.js
+++ src/contrainers/About.js
@@ -9,7 +9,7 @@ class Abouts extends React.Component {
             <div>
                 <Header />
                 <div className="mt-2 pt-5">
-                    <div className="text-center mt-5 mb-5 font-weight-bold">
+                    <div className="text-center mt-5 mb-5 font-weight-bold title">
                         <h1>Thank You</h1>
                     </div>
                     <hr />
